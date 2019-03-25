10b4f0746e70d45740f26fb3dd57e1ba3c2341ef
Berker ACIR <berkeracir159@gmail.com>
2019-03-25 08:21:33 +0300

    modify precomputing of histograms

--- hw1/precompute_histograms.py
+++ hw1/precompute_histograms.py
@@ -1,59 +1,110 @@
 import os
 import sys
-import glob
 import cv2
+import math
 import numpy as np
-import grayscale_histogram as gh
-import color_histogram as ch
 
-DATA_PATH = "data"
+import color_histogram as color_hist
+import gradient_histogram as grad_hist
+import grayscale_histogram as gray_hist
+
 DATASET_PATH = "dataset"
+CACHE_PATH = "cache"
 
-GRAYSCALE = "grayscale"
-COLOR = "color"
-GRADIENT = "gradient"
+def usage():
+    sys.stderr.write("Usage: python " + sys.argv[0] + " image_dataset.dat\n")
+    sys.exit(1)
 
-QUANTIZATION_LEVELS = [256, 128, 64, 32, 16] # Possible Bins: [8, 4, 2, 1]
-GRID_LEVELS = [1, 2, 3] # Possible Levels: [1, 6]
+def squeeze_histogram(histogram):
+    if histogram.shape[1] == 1:
+        return histogram
 
-def precompute_grayscale_histograms(image_path):
-    image_name = image_path.split("/")[-1]
-    folder_name = image_name.split(".")[0]
+    if len(histogram.shape) == 2:
+        level = histogram.shape[0]
+        bin = histogram.shape[1]
+        
+        retval = np.zeros((level, int(bin/2)))
 
-    if not os.path.exists(os.path.join(DATA_PATH, folder_name, GRAYSCALE)):
-        os.makedirs(os.path.join(DATA_PATH, folder_name, GRAYSCALE))
+        for i in range(bin):
+            retval[0][int(i/2)] += histogram[0][i]
 
-    for level in GRID_LEVELS:
-        for bin in QUANTIZATION_LEVELS:
-            image = cv2.imread(image_path)
-            ghist = gh.grayscale_histogram(image, level, bin)
-            fname = "l" + str(level) + "b" + str(bin)
-            fpath = os.path.join(DATA_PATH, folder_name, GRAYSCALE, fname)
-            np.save(fpath, ghist)
+        return retval
+    else:
+        level = histogram.shape[0]
+        bin = histogram.shape[1]
+        
+        retval = np.zeros((level, int(bin/2), int(bin/2), int(bin/2)))
 
-def precompute_3d_color_histograms(image_path):
-    image_name = image_path.split("/")[-1]
-    folder_name = image_name.split(".")[0]
+        for i in range(bin):
+            for j in range(bin):
+                for k in range(bin):
+                    retval[0][int(i/2)][int(j/2)][int(k/2)] += histogram[0][i][j][k]
 
-    if not os.path.exists(os.path.join(DATA_PATH, folder_name, COLOR)):
-        os.makedirs(os.path.join(DATA_PATH, folder_name, COLOR))
+        return retval
 
-    for level in GRID_LEVELS:
-        for bin in QUANTIZATION_LEVELS:
-            image = cv2.imread(image_path)
-            chist = ch.color_histogram(image, level, bin)
-            fname = "l" + str(level) + "b" + str(bin)
-            fpath = os.path.join(DATA_PATH, folder_name, COLOR, fname)
-            np.save(fpath, chist)
+def precompute_grayscale_histograms(image):
+    img = cv2.imread(os.path.join(DATASET_PATH, image))
+    img_name = image.split(".")[0]
 
-#def precompute_gradient_histograms(image_path):
+    level = 1
+    bin = 256
+
+    gray_histogram = gray_hist.grayscale_histogram(img, level, bin)
+
+    for i in range(int(math.log(bin, 2)) + 1):
+        conf = "gray_l" + str(level) + "b" + str(bin)
+
+        if not os.path.exists(os.path.join(CACHE_PATH, conf)):
+            os.makedirs(os.path.join(CACHE_PATH, conf))
+
+        np.save(os.path.join(CACHE_PATH, conf, img_name), gray_histogram)
+
+        gray_histogram = squeeze_histogram(gray_histogram)
+        bin = int(bin/2)
+
+def precompute_color_histograms(image):
+    img = cv2.imread(os.path.join(DATASET_PATH, image))
+    img_name = image.split(".")[0]
+
+    level = 1
+    bin = 64
+
+    color_histogram = color_hist.color_histogram(img, level, bin)
+
+    for i in range(int(math.log(bin, 2)) + 1):
+        conf = "color_l" + str(level) + "b" + str(bin)
+
+        if not os.path.exists(os.path.join(CACHE_PATH, conf)):
+            os.makedirs(os.path.join(CACHE_PATH, conf))
+
+        np.save(os.path.join(CACHE_PATH, conf, img_name), color_histogram)
+
+        color_histogram = squeeze_histogram(color_histogram)
+        bin = int(bin/2)
+
+#def precompute_gradient_histograms(image):
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2: usage()
+
     if not os.path.exists(DATASET_PATH):
         sys.stderr.write("Dataset folder does not exist \""
                         + DATASET_PATH + "\"\n")
         sys.exit(1)
 
-    for img_path in glob.glob(os.path.join(DATASET_PATH, "*.jpg")):
-        #precompute_grayscale_histograms(img_path)
-        precompute_3d_color_histograms(img_path)
+    if not os.path.exists(CACHE_PATH):
+        os.makedirs(CACHE_PATH)
+
+    dataset_images = []
+
+    with open(sys.argv[1], "r") as dataset_file:
+        for dataset_line in dataset_file:
+            img_name = dataset_line.strip("\r\n")
+            dataset_images.append(img_name)
+
+    for image in dataset_images:
+        #precompute_grayscale_histograms(image) # DONE
+        precompute_color_histograms(image)
+        #precompute_gradient_histograms(image)
+
+        
